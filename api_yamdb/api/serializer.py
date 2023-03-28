a28d54d44e8f3102ddfefcff656b55ce57772fba
Bulat Gabdullin <byjiat@gmail.com>
2023-03-28 20:44:35 +0500

    Add validators in ReviewSerializer

--- api_yamdb/api/serializer.py
+++ api_yamdb/api/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
+from api_yamdb.api.validators import RangeValueValidator
 from api_yamdb.reviews.models import Review
 
 
@@ -12,3 +14,13 @@ class ReviewSerializer(serializers.ModelSerializer):
             'author',
             'pub_date',
         )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=['title_id', 'author']
+            ),
+            RangeValueValidator(
+                field='score',
+            ),
+
+        ]
