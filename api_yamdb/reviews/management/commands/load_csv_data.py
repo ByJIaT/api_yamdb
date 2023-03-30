33ec15c255fe33bc2ee5665d93849a109266fa82
Никита Седымов <nickitasedymov@yandex.ru>
2023-03-30 20:33:04 +0500

    csv before test

---
+++ api_yamdb/reviews/management/commands/load_csv_data.py
@@ -0,0 +1,41 @@
+from csv import DictReader
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+
+
+User = get_user_model()
+
+DATASET = {
+    User: 'users.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    GenreTitle: 'genre_title.csv',
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        try:
+            for model, file in DATASET.items():
+                with open(
+                    f'{settings.BASE_DIR}/static/data/{file}',
+                    mode='r',
+                    encoding='utf-8',
+                ) as f:
+                    reader = DictReader(f)
+                    model.objects.bulk_create(
+                        model(**data) for data in reader
+                    )
+                    f.close()
+            self.stdout.write('Данные успешно загружены')
+        except Exception as error:
+            self.stdout.write(
+                f'Возникла ошибка во время загрузки файла '
+                f'{file}: {error}'
+            )
